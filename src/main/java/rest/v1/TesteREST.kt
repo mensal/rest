3793658ff86b6e09d11d2a533331e196f6b054b8
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2018-04-02 18:58:40 -0300

    Testando a conectividade com o banco de dados

--- src/main/java/rest/v1/TesteREST.kt
+++ src/main/java/rest/v1/TesteREST.kt
@@ -1,14 +1,33 @@
 package rest.v1
 
-import java.time.Instant
+import core.entity.Teste
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import javax.transaction.Transactional
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 
 @Path("teste")
 internal open class TesteREST {
 
+    @PersistenceContext
+    private val em: EntityManager? = null
+
+    @POST
+    @Transactional
+    open fun post() {
+        em!!.persist(Teste())
+    }
+
     @GET
-    @Produces("text/plain")
-    open fun teste() = Instant.now()
+    @Produces("application/json")
+    open fun get(): List<Teste> {
+        val jpql = " from Teste "
+
+        val query = em!!.createQuery(jpql, Teste::class.java)
+        return query.resultList
+    }
+
 }
