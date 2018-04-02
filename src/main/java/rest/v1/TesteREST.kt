f77032a46f63e19e06795ba3dbee582ab52e2fcb
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2018-04-02 11:05:44 -0300

    Preparando a estrutura básica do projeto.

--- src/main/java/rest/v1/TesteREST.kt
+++ src/main/java/rest/v1/TesteREST.kt
@@ -3,10 +3,12 @@ package rest.v1
 import java.time.Instant
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
 
 @Path("teste")
 open class TesteREST {
 
     @GET
+    @Produces("text/plain")
     fun teste() = Instant.now()
 }
