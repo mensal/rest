5419af7b7068f56c0d79afa8eef08619dd14246a
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2018-04-13 10:47:47 -0300

    Script de carga das despesas fixas

---
+++ src/etc/db/tipo_fixas.sql
@@ -0,0 +1,30 @@
+INSERT INTO tipo_despesa (id, atualizado_em)
+VALUES ('b5fd2103-2dce-428f-87d2-fad7a5e68d89', now());
+
+INSERT INTO tipo_despesa_fixa (id, nome, vencimento)
+VALUES ('b5fd2103-2dce-428f-87d2-fad7a5e68d89', 'Telefone fixo', 5);
+
+INSERT INTO tipo_despesa (id, atualizado_em)
+VALUES ('3afaa279-cdd2-4cab-84bc-7b77b14ceba7', now());
+
+INSERT INTO tipo_despesa_fixa (id, nome, vencimento)
+VALUES ('3afaa279-cdd2-4cab-84bc-7b77b14ceba7', 'Condomínio', 5);
+
+INSERT INTO tipo_despesa (id, atualizado_em)
+VALUES ('db82e500-340b-41db-b247-a0f7a465ba83', now());
+
+INSERT INTO tipo_despesa_fixa (id, nome, vencimento)
+VALUES ('db82e500-340b-41db-b247-a0f7a465ba83', 'Eletricidade', 5);
+
+INSERT INTO tipo_despesa (id, atualizado_em)
+VALUES ('b15c0699-d86c-49a0-9475-3d4936edd5fd', now());
+
+INSERT INTO tipo_despesa_fixa (id, nome, vencimento)
+VALUES ('b15c0699-d86c-49a0-9475-3d4936edd5fd', 'Gás', 5);
+
+INSERT INTO tipo_despesa (id, atualizado_em)
+VALUES ('5695d83f-529b-4a91-acd7-79116033f301', now());
+
+INSERT INTO tipo_despesa_fixa (id, nome, vencimento)
+VALUES ('5695d83f-529b-4a91-acd7-79116033f301', 'Netflix', 5);
+
