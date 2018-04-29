e72058ce07f23534cfc9f7bf4b56a6961075c304
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2018-04-29 17:49:11 -0300

    Removendo o Probe e aumentando o cache do Preflight

--- src/main/java/rest/service/ProbeREST.kt
+++ src/main/java/rest/service/ProbeREST.kt
@@ -1,16 +1,16 @@
-package rest.service
-
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-
-@Path("probe")
-open class ProbeREST {
-
-    @GET
-    @Path("readiness")
-    open fun readiness() = null
-
-    @GET
-    @Path("liveness")
-    open fun liveness() = null
-}
+//package rest.service
+//
+//import javax.ws.rs.GET
+//import javax.ws.rs.Path
+//
+//@Path("probe")
+//open class ProbeREST {
+//
+//    @GET
+//    @Path("readiness")
+//    open fun readiness() = null
+//
+//    @GET
+//    @Path("liveness")
+//    open fun liveness() = null
+//}
