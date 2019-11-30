d70578c0d309a828dec600fdcd903a8f3a6c4c15
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2019-11-30 20:14:42 -0300

    Experimento de migração para o Spring Boot: Limpando a casa.

--- src/main/kotlin/app/rest/service/ProbeREST.kt
+++ src/main/kotlin/app/rest/service/ProbeREST.kt
@@ -1,20 +1,15 @@
 package app.rest.service
 
-import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/probe")
 class ProbeREST {
 
-//    @ResponseStatus(NO_CONTENT)
-//    @ResponseStatus(NO_CONTENT)
     @GetMapping("readiness")
     fun readiness(): Nothing? = null
-//    fun readiness() = "opaaaaaaa"
 
     @GetMapping("liveness")
     fun liveness(): Nothing? = null
